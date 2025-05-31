58cc363f463475e8c1e413b6c62561bda10b4f1b
Shaikh Shoeb <127790182+it-Shoeb@users.noreply.github.com>
2025-05-31 20:47:53 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ import cookieParser from "cookie-parser";
 import { configDotenv } from "dotenv";
 import cors from "cors";
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://invy-j2ow.onrender.com", credentials: true }));
 app.use(express.json());
 app.use(cookieParser()); // Must come after CORS
 app.use(express.urlencoded({ extended: true }));
