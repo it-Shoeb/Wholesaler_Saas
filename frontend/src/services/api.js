5c12778a7050ff2d1e65c88b82fb97517a085731
it-Shoeb <sshoeb577@gmail.com>
2025-06-05 00:15:40 +0530

    updated api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "http://localhost:3000",
-  baseURL: "https://invy-backend-3077.onrender.com",
+  baseURL: "https://invy-backend-ols7.onrender.com",
   timeout: 3000,
   withCredentials: true,
 });
