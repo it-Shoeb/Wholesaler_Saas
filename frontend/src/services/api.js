10e6ffe90aab95fcfdbe4a969916ef33196b72d7
it-Shoeb <sshoeb577@gmail.com>
2025-06-10 22:27:55 +0530

    update

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
-  // baseURL: "https://invy-backend-ols7.onrender.com",
+  // baseURL: "http://localhost:3000",
+  baseURL: "https://invy-backend-ols7.onrender.com",
   timeout: 5000,
   withCredentials: true,
 });
